9193e199fe87754453c2e4be4444044c7a67a0bd
Sam Ho <sam.ho@inspire-dt.com>
2022-03-25 15:16:57 +0800

    remove sider not necessary elements

--- src/components/common/Sider/index.js
+++ src/components/common/Sider/index.js
@@ -1,23 +1,17 @@
-import { useState } from 'react';
-import styled from 'styled-components';
 import { Layout, Menu } from 'antd';
 const { Sider } = Layout;
 // import { DesktopOutlined, PieChartOutlined, FileOutlined } from '@ant-design/icons';
 
 export default function Index({ menu }) {
   return (
-    <StyledSider
+    <Sider
       breakpoint={'lg'}
       theme="light"
       collapsedWidth={0}
       trigger={null}
     >
       {menu}
-    </StyledSider>
+    </Sider>
   );
 }
 
-const StyledSider = styled(Sider)`
-  height: 100%;
-  margin-right: 24px;
-`;
