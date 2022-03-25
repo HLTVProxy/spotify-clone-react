d2d6b5cfeb98f594ebd71ef2063f23b0bbf5e457
Sam Ho <sam.ho@inspire-dt.com>
2022-03-25 15:17:31 +0800

    rename component Index to Home

---
+++ src/components/content/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Layout } from 'antd';
+const { Content } = Layout;
+
+function Index() {
+  return (
+    <Layout>
+      <Content>Home</Content>
+    </Layout>
+  );
+}
+
+export default Index;
