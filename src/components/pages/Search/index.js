cfaac25835b8dacab9515698e18040cfef43846e
Sam Ho <sam.ho@inspire-dt.com>
2022-03-29 13:47:54 +0800

    rename folder

---
+++ src/components/pages/Search/index.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { Input } from 'antd';
+import styled from 'styled-components';
+import StyledContent from '../../common/Content';
+import { SearchOutlined } from '@ant-design/icons';
+function Search() {
+  const [search, setSearch] = useState('');
+  const [url, setUrl] = useState(window.location.href);
+
+  useEffect(() => {
+      window.history.pushState("", "", `${url}/${search}`);
+  }, [search]);
+
+  const handleSearch = (text) => {
+    setSearch(text);
+  };
+
+  return (
+    <StyledContent>
+      <SearchBar
+        placeholder="藝人、歌曲或 Podcast"
+        prefix={<SearchOutlined />}
+        allowClear
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        value={search}
+      />
+    </StyledContent>
+  );
+}
+
+export default Search;
+
+const SearchBar = styled(Input)`
+  width: 30%;
+  border-radius: 500px;
+  margin-top: 32px;
+`;
