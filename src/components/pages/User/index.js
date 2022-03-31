6767c03045c02f9202a20d24186c1d6a8a1a16a9
Sam Ho <sam.ho@inspire-dt.com>
2022-03-31 11:16:02 +0800

    add components toUser page

--- src/components/pages/User/index.js
+++ src/components/pages/User/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import InfoHeader from '../../common/InfoHeader';
+import CardList from '../../common/CardList'
+import SongList from '../../common/SongList';
 
 function User() {
   return (
-    <div>User</div>
-  )
+    <>
+      <InfoHeader type="user" description={false} />
+      <CardList title='本月最熱門藝人' type="artist"/>
+      <SongList title='本月最熱門曲目' />
+      <CardList title='粉絲' type="fans"/>
+      <CardList title='正在關注' type="follow"/>
+    </>
+  );
 }
 
-export default User
+export default User;
