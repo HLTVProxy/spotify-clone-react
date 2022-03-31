25a450d5681c295710e87b5989d4777feac97e76
Sam Ho <sam.ho@inspire-dt.com>
2022-03-31 16:14:31 +0800

    add collapse

--- src/components/common/HotTrackList/index.js
+++ src/components/common/HotTrackList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { List, Button } from 'antd';
 import { CaretRightOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
@@ -36,14 +36,22 @@ function HotTrackList() {
       title: '飛鳥和蟬0',
     },
   ];
+
+  const [collapse, setCollapse] = useState(false);
+
+  const handleCollapse = () => {
+    setCollapse(!collapse);
+  };
+
   return (
-    <StyledDiv>
+    <StyledDiv collapse={collapse}>
       <h1>熱門</h1>
       <List
         itemLayout="horizontal"
+        size="small"
         dataSource={data}
         renderItem={(item, idx) => (
-          <List.Item>
+          <List.Item className={idx > 4 ? 'collapse-item' : ''}>
             <Rank className="rank">
               <span className="rank-num">{idx + 1}</span>
               <PlayButton icon={<CaretRightOutlined />} size="large" />
@@ -52,12 +60,13 @@ function HotTrackList() {
               <img src="https://i.scdn.co/image/ab67616d00001e02dfb13a39f0976b75efd6bf84" />
             </div>
             <List.Item.Meta title={<a href="#!">{item.title}</a>} />
-            <div className="song-info">
-                123
-            </div>
+            <div className="song-info">123</div>
           </List.Item>
         )}
       />
+      <button className="collapse-track-btn" onClick={handleCollapse}>
+        {collapse ? '顯示較少內容' : '檢視更多'}
+      </button>
     </StyledDiv>
   );
 }
@@ -68,7 +77,6 @@ const StyledDiv = styled.div`
   padding-top: 16px;
 
   .rank {
-    padding-left: 16px;
     color: #fff;
   }
 
@@ -80,13 +88,17 @@ const StyledDiv = styled.div`
     width: 50px;
   }
 
+  .ant-list-split .ant-list-item {
+    border: none;
+  }
+
   .ant-list-item:hover {
-      .rank-num {
-          display:none;
-      }
-      button {
-          display: block;
-      }
+    .rank-num {
+      display: none;
+    }
+    button {
+      display: block;
+    }
   }
 
   .ant-list-item-meta-title > a {
@@ -100,7 +112,21 @@ const StyledDiv = styled.div`
   }
 
   .song-info {
-      color: #6a6a6a;
+    color: #6a6a6a;
+  }
+
+  .collapse-item {
+    display: ${(props) => (props.collapse ? 'flex' : 'none')};;
+  }
+
+  .collapse-track-btn {
+    background-color: transparent;
+    color: rgba(255, 255, 255, 0.7);
+    border: none;
+    padding-top: 16px;
+  }
+  .collapse-track-btn:hover {
+    color: #fff;
   }
 `;
 
