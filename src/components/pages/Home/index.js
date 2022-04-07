cf2780bec5493cf6ee25d9d70ae90c406bc23c64
Sam Ho <sam.ho@inspire-dt.com>
2022-04-07 17:10:02 +0800

    add props in Home Page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -6,9 +6,9 @@ import CardList from '../../common/CardList';
 function Home() {
   return (
     <>
-      <CardList title="最近播放" />
-      <CardList title="官方播放清單" />
-      <CardList title="猜你喜歡" />
+      <CardList title="最近播放" type="tracks" detailHref="/genre/recent-play" />
+      <CardList title="官方播放清單" type="playlists" detailHref="/genre/official-playlists" />
+      <CardList title="猜你喜歡" detailHref="/genre/recommendation"/>
     </>
   );
 }
