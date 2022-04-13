db0434295f487d906645671546b9647e391f9d41
Sam Ho <sam.ho@inspire-dt.com>
2022-04-13 17:43:10 +0800

    finish collection page

--- src/components/pages/Collection/Artists/index.js
+++ src/components/pages/Collection/Artists/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import CollectionTabs from '../CollectionTabs';
 import CardList from '../../../common/CardList';
+import apiClient from '../../../../spotify';
 
 function Artists() {
+  const [collectionArtists, setCollectionArtists] = useState([]);
+
+  useEffect(() => {
+    fetchCollectionArtists();
+  }, []);
+
+  const fetchCollectionArtists = () => {
+    apiClient
+      .get(`me/following?type=artist&limit=50`)
+      .then((res) => {
+        let artists = res.data.artists.items.map((artist) => {
+          return {
+            id: artist.id,
+            uri: artist.uri,
+            title: artist.name,
+            descriptions: '藝人',
+            coverUrl: artist.images[0]?.url,
+          };
+        });
+        setCollectionArtists(artists);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <CollectionTabs />
-      <CardList title="藝人" type="artist" detail={false} />
+      <CardList title="藝人" type="artists" detail={false}>{collectionArtists}</CardList>
     </>
   );
 }
--- src/components/pages/Collection/PlayLists/index.js
+++ src/components/pages/Collection/PlayLists/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import CollectionTabs from '../CollectionTabs';
 import CardList from '../../../common/CardList';
+import apiClient from '../../../../spotify';
 
 function PlayLists() {
+  const [collectionPlaylists, setCollectionPlaylists] = useState([]);
+
+  useEffect(() => {
+    fetchCollectionPlaylists();
+  }, []);
+
+  const fetchCollectionPlaylists = () => {
+    apiClient
+      .get(`/me/playlists`)
+      .then((res) => {
+        let playlists = res.data.items.map((playlist) => {
+          return {
+            id: playlist.id,
+            uri: playlist.uri,
+            title: playlist.name,
+            descriptions: playlist.description ? playlist.description : `來自 ${playlist.owner.display_name}`,
+            coverUrl: playlist.images[0]?.url,
+          };
+        });
+        setCollectionPlaylists(playlists);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <CollectionTabs />
-      <CardList title="播放清單" type="playlist" detail={false} />
+      <CardList title="播放清單" type="collectionPlaylist" detail={false}>{collectionPlaylists}</CardList>
     </>
   );
 }
